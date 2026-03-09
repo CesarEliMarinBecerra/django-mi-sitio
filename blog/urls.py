77aaa248b1c206685c11ff09c5ff25654b737182
CesarEliMarinBecerra <cesarelimarinb00@gmail.com>
2026-03-09 07:09:15 -0600

    Actualizacion del blog y preparacion para deploy

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='inicio'),
     path('proyectos/', views.proyectos, name='proyectos'),
     path('blog/', views.blog_view, name='blog'),
-    path('sobre-mi/', views.sobre_mi, name='sobre_mi'),
+    path('sobre_mi/', views.sobre_mi, name='sobre_mi'),
     path('certificados/', views.certificados, name='certificados'),
 
 ]
--- mi_portafolio/settings.py
+++ mi_portafolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$6!s7-+2yaa78zj#vth*xmzqwm%t)9kkle9dk3frm@gaj#hvl1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
